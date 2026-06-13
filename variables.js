3fa8009341c4eabf0d02a08a9414d42429601e79
Ahmad fahim <af684568@gmail.com>
2026-06-13 22:58:27 +0500

    learning variables in javascript

---
+++ variables.js
@@ -0,0 +1,12 @@
+const accountId =12345
+let accountEmail = "af684568gmail.com"
+var accountPassword = "password"
+accountCity="Peshawar"
+let accountState = "Khyber Pakhtunkhwa"
+
+
+accountEmail="ccccccc"
+console.log(accountId)
+// note dont used var because of scopes it leake a functions scopes and overrde many values sliently
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
