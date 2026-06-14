fdb528c55e57b2a20e68029dea82058236bc1cd1
Ahmad fahim <af684568@gmail.com>
2026-06-14 22:50:32 +0500

    New Topic learning

---
+++ StringToNumber.js
@@ -0,0 +1,23 @@
+let Value = 3
+let NegValue = -Value
+console.log(NegValue)
+
+
+console.log("1" + 2 + 3)
+console.log(1 + 2 + "3")
+
+let Str = "Hello"
+let Str1 = "World"
+console.log(Str + " " + Str1)
+
+
+let gameCounter =  100;
+++gameCounter
+console.log(gameCounter)
+
+
+
+
+let gameCounter1 = 100;
+gameCounter1++
+console.log(gameCounter1)
