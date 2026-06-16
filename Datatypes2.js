d4123cb55d53b26d4030dbac80caf1850b31281e
Ahmad fahim <af684568@gmail.com>
2026-06-16 22:37:45 +0500

    datartypes/Summary

---
+++ Datatypes2.js
@@ -0,0 +1,33 @@
+const Score = 100
+
+const Scorevalue = 100.3
+
+
+
+
+
+
+const isLoggedIn= true
+const outSide  = null
+
+
+
+const  id = Symbol("123")
+const  AnotherId = Symbol("123")
+
+console.log(id === AnotherId) // false because each symbol is unique
+
+
+const BigNumber = 9007199254740991n; // BigInt literal      
+console.log(BigNumber)
+console.log(typeof BigNumber) // "bigint"
+
+
+
+const heros =  ["Superman", "Batman", "Spiderman"]
+let myObj ={
+    name:"hitesh",
+    age: 22,
+}
+console.log(heros)
+console.log(myObj)
