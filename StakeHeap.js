6c8a0de8f23065d3e7e855bb8c82cdc78862a6a9
Ahmad fahim <af684568@gmail.com>
2026-06-17 22:39:30 +0500

    steckHeap  Learning from Yt

---
+++ StakeHeap.js
@@ -0,0 +1,18 @@
+let MyName = "Ahmad Fahim"
+let AnotherName = MyName
+AnotherName = "Ahmad Fahim Khan"
+
+console.log(MyName)
+console.log(AnotherName)
+
+
+let UserOne = {
+    email:"Ahmadfahim3456@gmail.com",
+    password:"123456789",
+
+}
+
+let userTwo = UserOne
+
+console.log(UserOne.email)
+console.log(userTwo.email)
