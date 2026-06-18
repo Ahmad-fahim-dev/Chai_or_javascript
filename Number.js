1c13727230cab7c223dea7aa6cbfc983aad2989c
Ahmad fahim <af684568@gmail.com>
2026-06-18 22:35:19 +0500

    About Numbers

---
+++ Number.js
@@ -0,0 +1,43 @@
+// Number and maths 
+const score  = 400
+console.log(score)
+
+
+const balance =  100
+console.log(balance)
+
+
+console.log(balance.toString())
+console.log(balance.toFixed(2))
+
+const OtherNumber = 23.33
+console.log(OtherNumber.toPrecision(7))
+
+
+const hundreds= 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+ 
+
+// Maths 
+console.log(Math.PI)
+
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.6));
+
+console.log(Math.ceil(4.2));
+
+console.log(Math.floor(4.8));
+
+
+let Number= Array=[1,2,3,4,5]
+console.log(Math.max(...Number))
+
+console.log(Math.min(...Number))
+
+console.log(Math.random(Number) *10)
+
+const min  = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min) + min))
