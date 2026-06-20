1c0ac452ec52acfefe80adeca17f942002064450
Ahmad fahim <af684568@gmail.com>
2026-06-20 22:23:50 +0500

    About arrays

---
+++ array.js
@@ -0,0 +1,41 @@
+//  array
+
+
+// shallow copy 
+// deep copy
+const myArr =[0,1,2,3,4,5]
+const myHeors = ["superman","Spiderman","batman"]
+
+
+console.log(myArr[0])
+
+
+const MyArr2 = new Array(1,2,3,4,5)
+console.log(myArr[1])
+
+
+//  Array methods
+
+
+myArr(push(6))
+MyArr2(pop())   // reomove last values form array
+console.log(myArr)
+
+console.log(myArr.includes(9));
+
+console.log(myArr.indexOf(3));
+
+const NewArray = myArr.join()
+
+console.log(myArr);
+
+console.log(NewArray)
+
+const mya1 = myArr.slice(1,3)
+
+console.log(mya1)
+
+
+const mya2 = myArr.splice(1,3)
+console.log(mya2)
+
